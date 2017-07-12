8b839ee801c0edf89e44134d730dcfc18d88134d
ealejeASC4 <edgar.alejandro106@gmail.com>
2017-07-12 10:24:31 -0700

    i like doge

---
+++ pokemonfunction.js
@@ -0,0 +1,18 @@
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var alphabetLength = alphabet.length
+function randLetter(index) {
+    var randomIndex = Math.random() * index;
+    var result = Math.floor(randomIndex);
+     return result;
+}
+
+console.log(alphabet[randLetter(alphabet.length)]);
+
+
+var randLength
+
+function randWord(){  
+    for (randLength i=0; i<27; i++){
+        Console.log()
+    }
+}
