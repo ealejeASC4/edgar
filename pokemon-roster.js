294ae0b44ae08e18fb8cfecff1e5018bcdf218d2
ealejeASC4 <edgar.alejandro106@gmail.com>
2017-07-13 09:03:33 -0700

    this is java script

---
+++ pokemon-roster.js
@@ -0,0 +1,57 @@
+var pokemonRoster =
+[
+    [
+        ["Charizard"],
+        ["fire", "flying"],
+        [78],
+        [78],
+        [84],
+        [false],
+    ],
+    
+    [
+        ["Genesect"],
+        ["steel","bug"],
+        [71],
+        [95],
+        [120],
+        [true],
+    ],
+    
+    [
+        ["Pikachu"], 
+        ["Electric"], 
+        [35], 
+        [40], 
+        [55], 
+        [False],
+    ],
+    
+    [
+        ["metagross"], 
+        ["steel", "phychic"], 
+        [80], 
+        [130],   
+        [135], 
+        [False],
+    ],
+    
+    [
+        ["Sandslash"], 
+        ["Ground"], 
+        [75], 
+        [110], 
+        [100], 
+        [False],
+    ],
+
+    [
+        ["Rayquaza"] ,
+        ["flying", "dragon"] ,
+        [105],
+        [90],
+        [150],
+        [true],
+    ],
+]
+
