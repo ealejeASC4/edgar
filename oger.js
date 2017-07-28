c436e002808add811c5e8abff687380241a4bacc
ealejeASC4 <edgar.alejandro106@gmail.com>
2017-07-28 14:50:53 -0400

    hello

---
+++ oger.js
@@ -0,0 +1,13 @@
+var HP = 100;
+
+
+function setup(){
+$('body').append("<h1>Welcome to oger game</h1>");
+$('body').append("<div><h3>Stats</h3><p id='stat'></p></div>");
+$('body').append('<button>Attack</button>');
+$('body').append('')
+}
+
+
+//waiting till doc ready
+$(document).ready(setup); 
