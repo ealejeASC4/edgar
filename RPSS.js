120330dcaf7744f594a026439f45abed288da0c7
ealejeASC4 <edgar.alejandro106@gmail.com>
2017-08-07 13:32:25 -0400

    updated rock paper scissors

---
+++ RPSS.js
@@ -0,0 +1,72 @@
+var choices = ["rock","scissors","paper"];
+//var playerTwo = ["rock","paper","scissors"];
+// var randomChoices = Math.floor(Math.random()*3)//only meant for the computer now 
+//all the HTML elements 
+
+
+
+var p1 = document.getElementById("chooserock").innerHTML = player1[0];
+  $( "#chooserock" ).click(function() {
+  $("body").append("<p>you chose rock</p>")
+});
+
+
+
+
+
+
+// var p1Choice = choices[randomChoices]; // rock
+// var p2Choice = choices[randomChoices]; //scissors
+
+// if (p1Choice == p2Choice){
+//     console.log("a tie");
+// }else if (p1Choice === choices[1]){
+//     if (p2Choice === choices[0]){
+ 
+
+
+//         console.log("player 1 loses")
+//         console.log("rock beats scissors")
+
+//     }else {
+
+
+
+
+//         console.log("players 1 wins")
+//         console.log("scissors beats paper")
+//     }
+// }
+// else if (p1Choice === choices[2]){
+//     if (p2Choice === choices[0]){
+
+
+
+//         console.log("player 1 wins")
+//         console.log("paper beats rock")
+
+//     }else {
+
+
+//         console.log("player 1 loses")
+//         console.log("scissors beats paper")
+//     }
+// }
+// else if (p1Choice === choices[2]){
+//     if (p2Choice === choices[1]){
+
+
+//         console.log("player 1 loses")
+//         console.log("scissors beats paper ")
+
+//     }else {
+
+
+//         console.log("player 1 wins")
+//         console.log("paper beats rock")
+//     }
+// }
+
+
+
+
--- nestedAPI/nest.js
+++ nestedAPI/nest.js
@@ -1,3 +1,5 @@
+
+
 $.ajax({
   url: 'https://randomuser.me/api/',
   dataType: 'json',
@@ -9,23 +11,23 @@ $.ajax({
     var gend = data.results[0].gender
     var pic = data.results[0].picture.large
     var cont = data.results[0].nat
-
-    $('body').append(`<img src='${pic}'> `)
-    $('body').append(`<h2>Good Morning: ${title} ${first} ${last} </h2>`)
+console.log(cont)
+    $('body').append(`<div class="dateImg"><img src='${pic}'></div> `)
+    $('body').append(`<h2>You Have met: ${title} ${first} ${last} </h2>`)
     $('body').append(`<h2> gender: ${gend} </h2>`)
     
 
 
 
 $.ajax({
-    url: 'https://restcountries.eu/rest/v2/alpha?codes=nl',
+    url: 'https://restcountries.eu/rest/v2/alpha?codes=' + cont,
     dataType: 'json',
     success: function(data){
         console.log(data)
         var flag = (data[0].flag)
 
          $('body').append(`<h2>This person is from  ${cont}</h2>`)
-        $("body").append(`<h1> This is the flag: <br> <img src=${flag} </h1>`)
+        $("body").append(`<h1> This is the flag of their country!: <br> <div class=imbro> <img src='${flag}'> </h1></div>`)
        
 
     }
@@ -33,3 +35,34 @@ $.ajax({
 
 }) 
 }});
+
+
+//signin and sign out function
+
+
+
+//called when successful user log in
+function onSignIn(googleUser) {
+    console.log('User signed in!');
+    var profile = googleUser.getBasicProfile();
+    //change userName text, img source, & email text based on profile
+    $(".userName").text(profile.getName());
+   $('.userImg').empty();
+    $("userImg").attr("src", profile.getImageUrl());
+    $(".email").text(profile.getEmail());
+}
+
+
+//called when "sign out" button clicked
+function onSignOut() {
+    //should sign user out and toggleHidden
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.')
+        //setting back to default
+        $(".userName").text("USER_NAME");
+        $('.userImg').empty();
+        $(".userImg").attr("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQABotzdF2pYgVkuDNfEHGT9cSur6qgF7q2vpsoZSCvEBOYQaxcZq3vyQ");
+        $(".email").text("example@example.com");
+    });
+}
