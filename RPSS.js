147cf4ef15c0bef4e5b2669f190a49f1ec832a0e
ealejeASC4 <edgar.alejandro106@gmail.com>
2017-08-08 14:44:34 -0400

    new Demo Day Added for the group

--- RPSS.js
+++ RPSS.js
@@ -1,20 +1,54 @@
 var choices = ["rock","scissors","paper"];
-//var playerTwo = ["rock","paper","scissors"];
-// var randomChoices = Math.floor(Math.random()*3)//only meant for the computer now 
-//all the HTML elements 
+var randomNum = Math.floor(Math.random()*3);
+var randomChoice = choices[randomNum];
+var didWin = false;
+var numOfTries = 0;
 
 
 
-var p1 = document.getElementById("chooserock").innerHTML = player1[0];
-  $( "#chooserock" ).click(function() {
-  $("body").append("<p>you chose rock</p>")
-});
+// function displayscore()
 
-
-
-
-
-
+function chooseChoice(value) {
+        if(value == "Rock" && randomChoice == "paper") {
+            $("#results").append("<p> The computer chose " + randomChoice + ". You lose ya loser 030</p>")
+        }
+        else if(value == "Rock" && randomChoice == "rock") {
+            $("#results").append("<p> The computer chose " + randomChoice + ". It's a tie RiP</p>")
+            
+        }
+        else if(value == "Rock" && randomChoice == "scissors") {
+            $("#results").append("<p> The computer chose " + randomChoice + ". You win GG</p>")
+            didWin = true;
+        }
+        else if(value == "Paper" && randomChoice == "paper") {
+            $("#results").append("<p> The computer chose " + randomChoice + ". It's a tie Hah try again</p>")
+        }
+        else if(value == "Paper" && randomChoice == "scissors") {
+            $("#results").append("<p> The computer chose " + randomChoice + ". You lose ya scrub</p>")
+        }
+        else if(value == "Paper" && randomChoice == "rock") {
+            $("#results").append("<p> The computer chose " + randomChoice + ". You win Ya meme lord</p>")
+            didWin = true;
+        }
+        else if(value == "Scissors" && randomChoice == "rock") {
+            $("#results").append("<p> The computer chose " + randomChoice + ". You lose kek</p>")
+        }
+        else if(value == "Scissors" && randomChoice == "paper") {
+            $("#results").append("<p> The computer chose " + randomChoice + ". You win heheh the best </p>")
+            didWin = true;
+        }
+        else {
+            $("#results").append("<p> The computer chose " + randomChoice + ". It's a tie hehe go again</p>")
+        }
+    randomNum = Math.floor(Math.random()*3);
+    randomChoice = choices[randomNum];
+    numOfTries = numOfTries + 1;
+    if(didWin) {
+        $("#results").append("<p>It took you " + numOfTries + " tries.</p>");
+        var voteB =  document.getElementById("buttons");
+        voteB.style.display='none';
+    }
+}
 // var p1Choice = choices[randomChoices]; // rock
 // var p2Choice = choices[randomChoices]; //scissors
 
